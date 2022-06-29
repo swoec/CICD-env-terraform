574e505f5acead7484f9db18a2deb577d174ac0b
swoec <swoec.wang@gmail.com>
2022-06-29 12:32:38 +1200

    init

---
+++ dev-env/vars.tf
@@ -0,0 +1,25 @@
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "ap-southeast-2"
+}
+
+
+variable "INSTANCE_DEVICE_NAME" {
+  default = "/dev/xvdh"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
